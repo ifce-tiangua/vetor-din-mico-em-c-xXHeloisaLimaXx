3a08f162d3d613b74cb147a8cb70ea638988dc64
xXHeloisaLimaXx <Helo.araujw@gmail.com>
2024-11-19 11:16:20 -0300

    Atualizar o main.c

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main() {
+    int *vetor;
+    int tamanho;
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    scanf("%d", &tamanho);
 
+    if (tamanho == 0) {
+        printf("[vazio]");
+        return 0;
+    }
+
+    vetor = malloc(tamanho * sizeof(int));
+    if (vetor == NULL) {
+        printf("Erro: memória insuficiente.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < tamanho; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    printf("[");
+    printf("%d", vetor[0]);
+    for (int i = 1; i < tamanho; i++) {
+        printf(", %d", vetor[i]);
+    }
+    printf("]");
+
+    free(vetor);
     return 0;
 }
